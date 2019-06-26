62f8974322c652c61757cbcc5d25460956aa6d63
Amador Pahim <amador@pahim.org>
2019-06-27 00:52:26 +0200

    Adding debug messages to the hunter
    Signed-off-by: Amador Pahim <amador@pahim.org>

--- wordhunter/core/hunter.py
+++ wordhunter/core/hunter.py
@@ -2,6 +2,8 @@
 Main Word Hunter module. This is where the magic happens.
 """
 
+import logging
+
 
 class Hunter:
     """
@@ -14,75 +16,109 @@ class Hunter:
     """
 
     def __init__(self, wordlist, matrix):
+        self.log = logging.getLogger('wordhunter')
+
         self.wordlist = wordlist
         self.matrix = matrix
 
         self.dimension = len(matrix)
         self.total_items = self.dimension ** 2
 
-        self.matches = set()
-
     def run(self):
         """
         Calls all the hunting helpers.
 
-        :return: matched words
+        :return: matched unique words
+        :rtype: set
         """
-        self._hunt_horizontal()
-        self._hunt_vertical()
-        self._hunt_diagonal_from_right()
-        self._hunt_diagonal_from_left()
-        return self.matches
+        matches = []
+        matches.extend(self._hunt_horizontal())
+        matches.extend(self._hunt_vertical())
+        matches.extend(self._hunt_diagonal_from_right())
+        matches.extend(self._hunt_diagonal_from_left())
+        return set(matches)
 
     def _hunt_horizontal(self):
         """
         Searches for words horizontally, both
         from left to right and from right to left
         """
+        self.log.debug('Horizontal search')
+
+        matches = []
+        rows = []
+
         for a in range(self.dimension):
+            rows.append([(a, b) for b in range(self.dimension)])
+
+        self.log.debug('  Rows:')
+        for row in rows:
+            self.log.debug('  --> %s', row)
             word = ''
-            for b in range(self.dimension):
-                word += self.matrix[a][b]
+            for coord in row:
+                word += self.matrix[coord[0]][coord[1]]
                 for x in range(len(word)):
                     temp_word = word[x:]
                     try:
                         self.wordlist[temp_word]
-                        self.matches.add(temp_word)
+                        matches.append(temp_word)
                     except KeyError:
                         pass
                     try:
                         self.wordlist[temp_word[::-1]]
-                        self.matches.add(temp_word[::-1])
+                        matches.append(temp_word[::-1])
                     except KeyError:
                         pass
+
+        self.log.debug('  Matches:')
+        self.log.debug('  --> %s', matches)
+        return matches
 
     def _hunt_vertical(self):
         """
         Searches for words vertically, both
         from top to bottom and from bottom to top
         """
+        self.log.debug('Vertical search')
+
+        matches = []
+        columns = []
+
         for a in range(self.dimension):
+            columns.append([(b, a) for b in range(self.dimension)])
+
+        self.log.debug('  Columns:')
+        for column in columns:
+            self.log.debug('  --> %s', column)
             word = ''
-            for b in range(self.dimension):
-                word += self.matrix[b][a]
+            for coord in column:
+                word += self.matrix[coord[0]][coord[1]]
                 for x in range(len(word)):
                     temp_word = word[x:]
                     try:
                         self.wordlist[temp_word]
-                        self.matches.add(temp_word)
+                        matches.append(temp_word)
                     except KeyError:
                         pass
                     try:
                         self.wordlist[temp_word[::-1]]
-                        self.matches.add(temp_word[::-1])
+                        matches.append(temp_word[::-1])
                     except KeyError:
                         pass
+
+        self.log.debug('  Matches:')
+        self.log.debug('  --> %s', matches)
+        return matches
 
     def _hunt_diagonal_from_right(self):
         """
         Searches for words in the diagonals from the top right to the
         bottom left
         """
+        self.log.debug('Diagonal search, from top right to bottom left')
+
+        matches = []
+
         a_start = 0
         a_end = 0
         b_start = self.dimension
@@ -113,7 +149,9 @@ class Hunter:
             diagonals.append([(next(a_iter), next(b_iter))
                               for _ in range(diagonal_len)])
 
+        self.log.debug('  Diagonals:')
         for diagonal in diagonals:
+            self.log.debug('  --> %s', diagonal)
             word = ''
             for coord in diagonal:
                 word += self.matrix[coord[0]][coord[1]]
@@ -121,20 +159,27 @@ class Hunter:
                     temp_word = word[x:]
                     try:
                         self.wordlist[temp_word]
-                        self.matches.add(temp_word)
+                        matches.append(temp_word)
                     except KeyError:
                         pass
                     try:
                         self.wordlist[temp_word[::-1]]
-                        self.matches.add(temp_word[::-1])
+                        matches.append(temp_word[::-1])
                     except KeyError:
                         pass
+        self.log.debug('  Matches:')
+        self.log.debug('  --> %s', matches)
+        return matches
 
     def _hunt_diagonal_from_left(self):
         """
         Searches for words in the diagonals from the top left to the
         bottom right
         """
+        self.log.debug('Diagonal search, from top left to bottom right')
+
+        matches = []
+
         start = 0
         end = 0
         diagonals = []
@@ -158,7 +203,9 @@ class Hunter:
             diagonals.append([(next(a_iter), next(b_iter))
                               for _ in range(diagonal_len)])
 
+        self.log.debug('  Diagonals:')
         for diagonal in diagonals:
+            self.log.debug('  --> %s', diagonal)
             word = ''
             for coord in diagonal:
                 word += self.matrix[coord[0]][coord[1]]
@@ -166,11 +213,14 @@ class Hunter:
                     temp_word = word[x:]
                     try:
                         self.wordlist[temp_word]
-                        self.matches.add(temp_word)
+                        matches.append(temp_word)
                     except KeyError:
                         pass
                     try:
                         self.wordlist[temp_word[::-1]]
-                        self.matches.add(temp_word[::-1])
+                        matches.append(temp_word[::-1])
                     except KeyError:
                         pass
+        self.log.debug('  Matches:')
+        self.log.debug('  --> %s', matches)
+        return matches
