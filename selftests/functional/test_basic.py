aaea7fec12defaa4f5c1196d099840d6b91c070b
Amador Pahim <amador@pahim.org>
2019-06-26 11:27:54 +0200

    Adding support for wordlist command line argument
    Signed-off-by: Amador Pahim <amador@pahim.org>

--- selftests/functional/test_basic.py
+++ selftests/functional/test_basic.py
@@ -1,6 +1,7 @@
 import re
 import subprocess
 
+from pathlib import Path
 from unittest import TestCase
 
 
@@ -16,3 +17,15 @@ class TestBasic(TestCase):
         output = self.run_command(args).stdout.decode().rstrip()
         pattern = r'^\d+.\d+.\d+$'
         self.assertTrue(re.match(pattern, output))
+
+    def test_wordlist_valid(self):
+        data = str(Path(__file__).parent.parent / 'data' / 'words.txt')
+        args = ['--wordlist', data]
+        result = self.run_command(args)
+        self.assertEquals(result.returncode, 0)
+        self.assertIn('Wordlist number of words: 62799', result.stdout.decode())
+
+    def test_wordlist_invalid(self):
+        args = ['--wordlist', 'invalid_path/words.txt']
+        result = self.run_command(args)
+        self.assertNotEquals(result.returncode, 0)
--- wordhunter/cli/cli.py
+++ wordhunter/cli/cli.py
@@ -3,14 +3,48 @@ Command Line Interface entry-point
 """
 
 import argparse
+import logging
+import sys
+
+from pathlib import Path
 
 from wordhunter.core.version import VERSION
+
+
+def _validate_wordlist(value):
+    """
+    Helper function to validate the wordlist file.
+    :param value: the path to the wordlist file
+    :return: the pathlib.Path instance of the wordlist file
+    """
+    wordlist_file = Path(value)
+    if not wordlist_file.is_file():
+        raise argparse.ArgumentTypeError(f'File {value} not found.')
+
+    return wordlist_file
 
 
 def main():
     """
     Parses the command line arguments and runs the application
     """
+    log = logging.getLogger(__name__)
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setFormatter(logging.Formatter(fmt='%(message)s'))
+    log.addHandler(handler)
+    log.setLevel(logging.INFO)
+
     parser = argparse.ArgumentParser(prog='wordhunter',
                                      description='Word Hunter')
     parser.add_argument('-v', '--version', action='version', version=VERSION)
+    parser.add_argument('-w', '--wordlist', required=True,
+                        type=_validate_wordlist,
+                        help='The file with the list of '
+                             'words to search for matches on.')
+    args = parser.parse_args()
+
+    wordlist_dict = dict()
+    with open(args.wordlist) as file_obj:
+        for line in file_obj:
+            wordlist_dict[line.rstrip()] = None
+    log.info('Wordlist number of words: %s', len(wordlist_dict))
